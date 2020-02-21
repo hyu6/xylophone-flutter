013c3cd7fcc508e82471a6bb39470d186a74b0ce
Haoran Yu <haoranyu6@gmail.com>
2020-02-21 02:25:54 -0800

    Expand keys

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,43 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum) {
+    final assetsAudioPlayer = AssetsAudioPlayer();
+
+    assetsAudioPlayer.open(
+      "assets/audios/note$soundNum.wav",
+    );
+    assetsAudioPlayer.play();
+  }
+
+  Expanded buildKey(Color color, int soundNum) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNum);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final assetsAudioPlayer = AssetsAudioPlayer();
-
-                assetsAudioPlayer.open(
-                  "assets/audios/note1.wav",
-                );
-                assetsAudioPlayer.play();
-              },
-              child: Text('Click'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.lightBlue, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.deepPurple, 7),
+            ],
           ),
         ),
       ),
