5f60402e2507b1da69c5b3337d12c18a2f80b3a5
Haoran Yu <haoranyu6@gmail.com>
2020-02-21 01:59:39 -0800

    Add asset audio player

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,7 +9,19 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              onPressed: () {
+                final assetsAudioPlayer = AssetsAudioPlayer();
+
+                assetsAudioPlayer.open(
+                  "assets/audios/note1.wav",
+                );
+                assetsAudioPlayer.play();
+              },
+              child: Text('Click'),
+            ),
+          ),
         ),
       ),
     );
